67e8312edbd763a620b6d130d20ed7232005289b
lsuinl <su96in43@naver.com>
2023-04-06 22:09:45 +0900

    init

---
+++ notice_with_me/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:notice_with_me/screen/login_screen.dart';
+void main() {
+  runApp(
+    MaterialApp(
+      home: LodinScreen()
+    ),
+  );
+}
