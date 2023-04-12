d249336df8aed719e39511b64c723d9b5df9e5d3
lsuinl <su96in43@naver.com>
2023-04-12 21:44:54 +0900

    중복삭제

--- frontend/notice_with_me/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:notice_with_me/screen/login_screen.dart';
-void main() {
-  runApp(
-    MaterialApp(
-      home: LodinScreen()
-    ),
-  );
-}
